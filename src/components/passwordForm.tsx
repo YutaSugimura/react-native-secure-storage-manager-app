52316f9cf5571d44f64e81dd07368240d246c34a
YutaSugimura <nextc3m@gmail.com>
2022-08-12 15:04:11 +0900

    fix: passwordForm.tsx

--- src/components/passwordForm.tsx
+++ src/components/passwordForm.tsx
@@ -1,10 +1,17 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 import {
   StyleSheet,
   TextInput,
   type TextInputProps,
   useColorScheme,
+  InputAccessoryView,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+  View,
 } from 'react-native';
+
+const inputAccessoryViewID = 'simple';
 
 type Props = TextInputProps & {
   focus?: boolean;
@@ -26,26 +33,53 @@ export const PasswordForm: React.FC<Props> = ({
     }
   }, [focus]);
 
+  const onCloseKeyboard = useCallback(() => {
+    ref.current?.blur();
+  }, []);
+
   return (
-    <TextInput
-      ref={ref}
-      value={value}
-      onChangeText={onChangeText}
-      onBlur={onBlur}
-      placeholder="password"
-      placeholderTextColor={isDarkTheme ? '#aaa' : '#ddd'}
-      style={[
-        styles.inputText,
-        isDarkTheme ? styles.inputText_dark : styles.inputText_light,
-        isDarkTheme ? styles.borderColor_dark : styles.borderColor_light,
-      ]}
-      autoCorrect={false}
-      autoCapitalize="none"
-      autoComplete="password"
-      returnKeyType="done"
-      secureTextEntry={true}
-      onSubmitEditing={onSubmitEditing}
-    />
+    <>
+      <TextInput
+        inputAccessoryViewID={inputAccessoryViewID}
+        ref={ref}
+        value={value}
+        onChangeText={onChangeText}
+        onBlur={onBlur}
+        placeholder="password"
+        placeholderTextColor={isDarkTheme ? '#aaa' : '#ddd'}
+        style={[
+          styles.inputText,
+          isDarkTheme ? styles.inputText_dark : styles.inputText_light,
+          isDarkTheme ? styles.borderColor_dark : styles.borderColor_light,
+        ]}
+        autoCorrect={false}
+        autoCapitalize="none"
+        autoComplete="password"
+        returnKeyType="done"
+        secureTextEntry={true}
+        onSubmitEditing={onSubmitEditing}
+      />
+
+      <InputAccessoryView nativeID={inputAccessoryViewID}>
+        <View
+          style={[
+            styles.accessoryContainer,
+            isDarkTheme && styles.accessoryContainer_dark,
+          ]}>
+          <TouchableOpacity
+            onPress={onCloseKeyboard}
+            style={styles.accessoryButton}>
+            <Text
+              style={[
+                styles.accessoryButtonLabel,
+                isDarkTheme && styles.accessoryButtonLabel_dark,
+              ]}>
+              Close
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </InputAccessoryView>
+    </>
   );
 };
 
@@ -72,5 +106,44 @@ const styles = StyleSheet.create({
   inputText_dark: {
     backgroundColor: '#353535',
     color: '#fff',
+  },
+  accessoryContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    height: 40,
+    width: Dimensions.get('screen').width,
+    backgroundColor: '#eee',
+    borderTopWidth: 1,
+    borderBottomWidth: 0.1,
+    borderColor: '#ccc',
+    shadowColor: '#666',
+    shadowOffset: {
+      width: 0,
+      height: -2,
+    },
+    shadowOpacity: 0.21,
+    shadowRadius: 3.6,
+    elevation: 5,
+  },
+  accessoryContainer_dark: {
+    backgroundColor: '#353535',
+    borderColor: '#aaa',
+    shadowColor: '#bbb',
+  },
+  accessoryButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  accessoryButtonLabel: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  accessoryButtonLabel_dark: {
+    color: '#eee',
   },
 });
