8193986aa38cc214ed5084c7adb9bb06d7522150
YutaSugimura <nextc3m@gmail.com>
2022-09-06 14:56:54 +0900

    feat: update to react-native@0.70.0

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,23 @@
 
 import React from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {AuthProvider} from './context/auth';
 import Navigator from './navigations';
 
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
     <AuthProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
 
       <Navigator />
     </AuthProvider>
